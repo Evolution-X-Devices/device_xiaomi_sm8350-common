8b78853a84f4778b8d29678b4869fe5153af7897
Joey Huab <jhuab@edukasyon.ph>
2025-01-01 14:13:29 +0900

    Revert "sm8350-common: Remove elf files in copy files check"
    This reverts commit a61c4fe556fa83545df6cd75095cc8cc7d228ae5.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -8,6 +8,10 @@ COMMON_PATH := device/xiaomi/sm8350-common
 
 BOARD_VENDOR := xiaomi
 
+# Ignore overriding commands errors
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # A/B
 AB_OTA_PARTITIONS += \
     boot \
