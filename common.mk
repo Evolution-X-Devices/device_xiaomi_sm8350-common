c926702c11ac13614db0febcb49fa26fb092e5ab
Joey Huab <jhuab@edukasyon.ph>
2025-08-19 12:09:53 +0900

    Revert "[SQUASH] sm8350-common: fastcharge implementation"
    This reverts commit d2793a3f48ad0a5b92c3b86c5c8f9b2c14779ccc.

--- common.mk
+++ common.mk
@@ -166,10 +166,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# Fastcharge
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.venus
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi
